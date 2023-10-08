44892d20fb41a3d2909beae4d50363f0a2a8f257
fdevos1 <felipe.devosm@gmail.com>
2023-10-08 14:35:20 -0300

    create project

---
+++ src/test/java/com/felipedevos/apirestfulmongodb/ApiRestfulMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipedevos.apirestfulmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
