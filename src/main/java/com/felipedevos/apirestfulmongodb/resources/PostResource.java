b1a08330aa596558e96448775bb20036b4d1f6fe
fdevos1 <felipe.devosm@gmail.com>
2023-10-09 10:24:40 -0300

    full search query with mongo params

--- src/main/java/com/felipedevos/apirestfulmongodb/resources/PostResource.java
+++ src/main/java/com/felipedevos/apirestfulmongodb/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.felipedevos.apirestfulmongodb.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,17 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/search")
+	public ResponseEntity<List<Post>> findPosts(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.findPosts(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
