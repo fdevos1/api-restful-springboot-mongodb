358dd685c7e35beb3ce2011009ad79f228d20823
fdevos1 <felipe.devosm@gmail.com>
2023-10-09 09:56:25 -0300

    added comments

--- src/main/java/com/felipedevos/apirestfulmongodb/config/Instantiation.java
+++ src/main/java/com/felipedevos/apirestfulmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.felipedevos.apirestfulmongodb.domain.Post;
 import com.felipedevos.apirestfulmongodb.domain.User;
 import com.felipedevos.apirestfulmongodb.dto.AuthorDTO;
+import com.felipedevos.apirestfulmongodb.dto.CommentDTO;
 import com.felipedevos.apirestfulmongodb.repository.PostRepository;
 import com.felipedevos.apirestfulmongodb.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("08/10/2023"), "Partiu viagem", "Vou viajar para São Paulo. Abraços",
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("09/10/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("08/10/2023"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("08/10/2023"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("09/10/2023"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
