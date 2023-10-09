a183e56c13c66578926445a37a55855a591fe6ec
fdevos1 <felipe.devosm@gmail.com>
2023-10-09 08:30:24 -0300

    users post method

--- src/main/java/com/felipedevos/apirestfulmongodb/services/UserService.java
+++ src/main/java/com/felipedevos/apirestfulmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipedevos.apirestfulmongodb.domain.User;
+import com.felipedevos.apirestfulmongodb.dto.UserDTO;
 import com.felipedevos.apirestfulmongodb.repository.UserRepository;
 import com.felipedevos.apirestfulmongodb.services.exception.ObjectNotFoundException;
 
@@ -15,17 +16,23 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
-		
-		
 
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-				
+
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
