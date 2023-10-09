c930ed0a960c42d7e8826ae0806221d9361ff2b4
fdevos1 <felipe.devosm@gmail.com>
2023-10-09 08:44:19 -0300

    users put method

--- src/main/java/com/felipedevos/apirestfulmongodb/services/UserService.java
+++ src/main/java/com/felipedevos/apirestfulmongodb/services/UserService.java
@@ -27,13 +27,27 @@ public class UserService {
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
-	public void delete (String id) {
-		
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+
+		updateData(newObj, obj);
+
+		return repo.save(newObj);
+
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(String id) {
+
 		findById(id);
 		repo.deleteById(id);
 	}
